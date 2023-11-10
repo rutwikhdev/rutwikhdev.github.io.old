2860fa4b98a14bec9dded4720df5ddfc1822b593
rutwik <rutwikhdev@gmail.com>
2023-11-11 01:11:20 +0530

    chore: Setup dark toggle on html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import './App.css'
+import { createEffect, createSignal } from 'solid-js'
+
+import Header from './components/Header'
 
 function App() {
+  const [darkMode, setDarkMode] = createSignal(false)
+  console.log(window.localStorage.getItem('DarkMode') === 'true')
+
+  if (window.localStorage.getItem('DarkMode') === 'true') {
+    setDarkMode(true)
+  }
+
+  createEffect(() => {
+    if (darkMode() === true) {
+      document.querySelector('html')!.classList.add('dark')
+    } else {
+      document.querySelector('html')!.classList.remove('dark')
+    }
+    window.localStorage.setItem('DarkMode', JSON.stringify(darkMode()))
+  })
 
   return (
     <>
-      Blog Init Bruv
+      <div
+        class={
+          (darkMode() ? 'dark-mode' : 'light-mode') +
+          ' h-screen flex flex-col justify-center items-center'
+        }
+      >
+        <p>
+        Blog Init Bruv
+        </p>
+
+        <button onclick={() => setDarkMode(!darkMode())}>
+          Dark Mode
+        </button>
+        <Header />
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./public/index.html",
+    "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
