e626fe1310db41b1c36e28df7ee9406fd72bd5ff
rutwik <rutwikhdev@gmail.com>
2023-11-10 23:42:21 +0530

    init: Blog init bruv

---
+++ README.md
@@ -0,0 +1,4 @@
+## Personal Blog Website
+
+### ToDo
+1. Render static markdown from /posts
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Blog Init Bruv
+    </>
+  )
+}
+
+export default App
